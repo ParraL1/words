c75a7db0f312ead4f0eaf5f11eb1eec2541ed604
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-02-23 21:41:33 -0600

    ok.0

--- words_in_both.py
+++ words_in_both.py
@@ -4,9 +4,9 @@
 #Description: sees what words are in both strings
 
 def words_in_both(s1, s2):
-    words1 = s1.lower().split(( ))
-    words2 = s2.lower().split(( ))
+    words1 = set(a.lower().spilit(()))
+    words2 = set(b.lower().split(( )))
     return words1.intersection(words2)
-    common_words = words_in_both(s1,s2)
+    common_words = words_in_both("jack of all trades",'His name is Jack')
 
 
